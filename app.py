b9ca5fb450338caabf003987863a10af16515e2b
Scuet <elizabeettt2606@gmail.com>
2026-08-31 22:11:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-flash')
 import os
 import json
 import time
